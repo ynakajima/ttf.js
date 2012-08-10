e5db22b75d67604baac53374175bf05eefbc0901
ynakajima <yuhta.nakajima@gmail.com>
2012-08-11 00:15:37 +0900

    Added: node.js demo

---
+++ demo/nodejs/ttfdump.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var ttfjs = require('./../../src/ttf.js');
+
+// check args
+if (process.argv.length < 3) {
+	console.error("usage: node ttfdump.js TTFfile" );
+	process.exit(1);
+}
+
+var path = process.argv[2];
+
+if (!fs.existsSync(path)) {
+	console.error("file not exists");
+	process.exit(1);
+}
+
+fs.readFile(path, function (err, data) {
+	if (err) throw err;
+	var ttf = new ttfjs.TTF(data);
+	console.dir(ttf);
+});
