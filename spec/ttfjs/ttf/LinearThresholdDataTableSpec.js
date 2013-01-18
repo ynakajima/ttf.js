71c4152a2d7dabf6ad3a63e244517f4b3638f685
ynakajima <yuhta.nakajima@gmail.com>
2013-01-19 05:57:37 +0900

    Add: Table mock classes

---
+++ spec/ttfjs/ttf/LinearThresholdDataTableSpec.js
@@ -0,0 +1,51 @@
+/**
+ * @license Released under the MIT license.
+ *
+ * <pre>
+ * Copyright (c) 2013 ynakajima <yuhta.nakajima@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * </pre>
+ *
+ * @fileoverview Spec of LinearThresholdDataTable Class.
+ * @author yuhta.nakajima@gmail.com (ynakajima)
+ */
+(function(global) {
+
+  // require
+  var ttfjs = (typeof require !== 'undefined') ?
+    require('../../../src/ttf/LinearThresholdDataTable.js') :
+    global.ttfjs;
+  var jDataView = (typeof global.jDataView === 'undefined') ?
+    require('../../../vendor/jdataview') :
+    global.jDataView;
+
+  // spec
+  describe('ttfjs.ttf.LinearThresholdDataTable', function() {
+
+    it('is Coonstructor', function() {
+      expect(ttfjs.ttf.LinearThresholdDataTable).toEqual(jasmine.any(Function));
+    });
+
+    // TODO(ynakajima) Add detail.
+
+  });
+
+})(this);
