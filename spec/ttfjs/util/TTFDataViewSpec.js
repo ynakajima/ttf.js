d8305869c21c2bb5274bb834878e999e2658004a
ynakajima <yuhta.nakajima@gmail.com>
2013-01-20 02:14:03 +0900

    Add: ttfjs.util.TTFDataView class

---
+++ spec/ttfjs/util/TTFDataViewSpec.js
@@ -0,0 +1,169 @@
+/**
+ * @license Released under the MIT license.
+ *
+ * <pre>
+ * Copyright (c) 2013 ynakajima <yuhta.nakajima@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * </pre>
+ *
+ * @fileoverview Spec of TTFDataView Class.
+ * @author yuhta.nakajima@gmail.com (ynakajima)
+ */
+(function(global) {
+
+  // require
+  var ttfjs = (typeof require !== 'undefined') ?
+    require('../../../src/util/TTFDataView.js') :
+    global.ttfjs;
+  var jDataView = (typeof global.jDataView === 'undefined') ?
+    require('../../../vendor/jdataview') :
+    global.jDataView;
+
+  // spec
+  describe('ttfjs.util.TTFDataView', function() {
+
+    var dummyBuffer = jDataView.createBuffer(
+      0xab, // BYTE: 171, CHAR: -85
+      0xde, 0xf0, // USHORT: 57072, SHORT: -8464
+      0xab, 0xcd, 0xef, 0x89, // ULONG: 2882400137, LONG: -1412567159
+      0x00, 0x01, 0x00, 0x00, // FIXED: 1.0
+      0x7f, 0xff, // F2DOT14: 1.999939
+      0x70, 0x00, // F2DOT14: 1.75
+      0x00, 0x01, // F2DOT14: 0.000061
+      0x00, 0x00, // F2DOT14: 0.0
+      0xff, 0xff, // F2DOT14: -0.000061
+      0x80, 0x00  // F2DOT14: -2.0
+    );
+    var view = new jDataView(dummyBuffer);
+    var ttfDataView = new ttfjs.util.TTFDataView(view);
+
+    it('is Coonstructor', function() {
+      expect(ttfjs.util.TTFDataView).toEqual(jasmine.any(Function));
+      expect(ttfDataView).toEqual(jasmine.any(ttfjs.util.TTFDataView));
+    });
+
+    it('has view member.', function() {
+      expect(ttfDataView.view).toEqual(jasmine.any(jDataView));
+    });
+
+    describe('ttfjs.util.TTFDataView.getByte()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getByte).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 8-bit unsigned integer.', function() {
+        expect(ttfDataView.getByte(0)).toEqual(171);
+      });
+
+    });
+
+    describe('ttfjs.util.TTFDataView.getChar()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getChar).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 8-bit signed integer.', function() {
+        expect(ttfDataView.getChar(0)).toEqual(-85);
+      });
+
+    });
+
+    describe('ttfjs.util.TTFDataView.getUshort()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getUshort).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 16-bit unsigned integer.', function() {
+        expect(ttfDataView.getUshort(1)).toEqual(57072);
+      });
+
+    });
+
+    describe('ttfjs.util.TTFDataView.getShort()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getShort).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 16-bit signed integer.', function() {
+        expect(ttfDataView.getShort(1)).toEqual(-8464);
+      });
+
+    });
+
+    describe('ttfjs.util.TTFDataView.getUlong()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getUlong).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 32-bit unsigned integer.', function() {
+        expect(ttfDataView.getUlong(3)).toEqual(2882400137);
+      });
+
+    });
+
+    describe('ttfjs.util.TTFDataView.getLong()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getLong).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 32-bit signed integer.', function() {
+        expect(ttfDataView.getLong(3)).toEqual(-1412567159);
+      });
+
+    });
+
+    describe('ttfjs.util.TTFDataView.getFixed()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getFixed).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 32-bit signed fixed-point number (16.16).', function() {
+        expect(ttfDataView.getFixed(7)).toEqual(1.0);
+      });
+
+    });
+
+    describe('ttfjs.util.TTFDataView.getF2dot14()', function() {
+
+      it('is method.', function() {
+        expect(ttfDataView.getF2dot14).toEqual(jasmine.any(Function));
+      });
+
+      it('returns the 16-bit signed fixed number with the low 14 bits of fraction (2.14).', function() {
+        expect(ttfDataView.getF2dot14(11)).toEqual(1.999939);
+        expect(ttfDataView.getF2dot14(13)).toEqual(1.75);
+        expect(ttfDataView.getF2dot14(15)).toEqual(0.000061);
+        expect(ttfDataView.getF2dot14(17)).toEqual(0.0);
+        expect(ttfDataView.getF2dot14(19)).toEqual(-0.000061);
+      });
+
+    });
+
+  });
+
+})(this);
---
+++ src/util/TTFDataView.js
@@ -0,0 +1,136 @@
+/**
+ * @license Released under the MIT license.
+ *
+ * <pre>
+ * Copyright (c) 2013 ynakajima <yuhta.nakajima@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * </pre>
+ *
+ * @fileoverview TTFDataView Class file.
+ * @author yuhta.nakajima@gmail.com (ynakajima)
+ */
+
+if (!ttfjs) { var ttfjs = {}; }
+
+(function(global, ttfjs) {
+
+  /** @namespace ttfjs.util */
+  if (typeof ttfjs.util === 'undefined') {
+    ttfjs.util = {};
+  }
+
+  // require
+  var jDataView = (typeof global.jDataView === 'undefined') ?
+    require('../../vendor/jdataview') :
+    global.jDataView;
+
+  /**
+   * TTFDataView Class
+   * @constructor
+   * @param {jDataView} jdataview jDataView instance.
+   */
+  ttfjs.util.TTFDataView = function(jdataview) {
+    /** @type {jDataView} */
+    this.view = jdataview;
+  };
+
+  /**
+   * Int2
+   * @type Array.<number>
+   */
+  ttfjs.util.TTFDataView.INT2 = [0, 1, -2, -1];
+
+  /**
+   * Return 8-bit unsigned integer.
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getByte = function(offset) {
+    return this.view.getUint8(offset | 0);
+  };
+
+  /**
+   * Return 8-bit signed integer.
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getChar = function(offset) {
+    return this.view.getInt8(offset | 0);
+  };
+
+  /**
+   * Return 16-bit unsigned integer.
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getUshort = function(offset) {
+    return this.view.getUint16(offset | 0);
+  };
+
+  /**
+   * Return 16-bit signed integer.
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getShort = function(offset) {
+    return this.view.getInt16(offset | 0);
+  };
+
+  /**
+   * Return 32-bit unsigned integer.
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getUlong = function(offset) {
+    return this.view.getUint32(offset | 0);
+  };
+
+  /**
+   * Return 32-bit signed integer.
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getLong = function(offset) {
+    return this.view.getInt32(offset | 0);
+  };
+
+  /**
+   * Return 32-bit signed fixed-point number (16.16).
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getFixed = function(offset) {
+    offset = offset | 0;
+    return parseInt(this.view.getUint16(offset).toString(10) +
+      '.' + this.view.getInt16(offset + 1).toString(10), 10);
+  };
+
+  /**
+   * Return 16-bit signed fixed number with the low 14 bits of fraction (2.14).
+   * @param {number} offset offset.
+   */
+  ttfjs.util.TTFDataView.prototype.getF2dot14 = function(offset) {
+    offset = offset | 0;
+    var raw = this.view.getUint16(offset);
+    var mantissa = ttfjs.util.TTFDataView.INT2[(raw >>> 14)];
+    var fraction = parseInt(('0000000000000000' + (raw).toString(2)).slice(-14), 2) / 16384;
+    return Math.round((mantissa + fraction) * 1000000) / 1000000;
+  };
+
+  // exports
+  if (typeof module !== 'undefined') {
+    module.exports = ttfjs;
+  }
+
+})(this, ttfjs);
