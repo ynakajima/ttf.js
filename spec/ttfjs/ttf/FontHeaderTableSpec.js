9f068143893a0bd726dc197ac3967fa0aa39664e
ynakajima <yuhta.nakajima@gmail.com>
2013-01-25 05:32:27 +0900

    Implementation: FontHeaderTable

--- spec/ttfjs/ttf/FontHeaderTableSpec.js
+++ spec/ttfjs/ttf/FontHeaderTableSpec.js
@@ -33,18 +33,166 @@
   var ttfjs = (typeof require !== 'undefined') ?
     require('../../../src/ttf/FontHeaderTable.js') :
     global.ttfjs;
+   ttfjs.Table = (typeof require !== 'undefined') ?
+    require('../../../src/Table.js').Table :
+    global.ttfjs.Table;
+   ttfjs.util = ttfjs.util || {};
+   ttfjs.util.TTFDataView = (typeof require !== 'undefined') ?
+    require('../../../src/util/TTFDataView.js').util.TTFDataView :
+    global.ttfjs.util.TTFDataView.js;
   var jDataView = (typeof global.jDataView === 'undefined') ?
     require('../../../vendor/jdataview') :
     global.jDataView;
+  var testFont = (typeof require !== 'undefined') ?
+    require('../../fonts/SourceCodePro-Medium.ttf.js') :
+    global.fontDataView;
 
   // spec
   describe('ttfjs.ttf.FontHeaderTable', function() {
+
+    var fontDataView = new ttfjs.util.TTFDataView(testFont);
+    var fontHeaderTable = ttfjs.ttf.FontHeaderTable.createFromDataView(fontDataView, 316);
 
     it('is Coonstructor', function() {
       expect(ttfjs.ttf.FontHeaderTable).toEqual(jasmine.any(Function));
     });
 
+    it('is inherited from Table.', function() {
+      expect(new ttfjs.ttf.FontHeaderTable()).toEqual(jasmine.any(ttfjs.Table));
+    });
+
+    describe('ttfjs.ttf.FontHeaderTable.createFromDataView', function() {
+
+      it('is static method.', function() {
+        expect(ttfjs.ttf.FontHeaderTable.createFromDataView).toEqual(jasmine.any(Function));
+        expect(ttfjs.ttf.FontHeaderTable.createFromDataView(fontDataView, 316)).toEqual(jasmine.any(ttfjs.ttf.FontHeaderTable));
+      });
+
+    });
+
+    it('has version', function() {
+      expect(fontHeaderTable.version).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.version).toEqual(1.0);
+    }); 
+
+    it('has fontRevision', function() {
+      expect(fontHeaderTable.fontRevision).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.fontRevision).toEqual(1.017);
+    }); 
+
+    it('has checkSumAdjustment', function() {
+      expect(fontHeaderTable.checkSumAdjustment).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.checkSumAdjustment).toEqual(0x6d0bc4b5);
+    }); 
+
+    it('has magicNumber', function() {
+      expect(fontHeaderTable.magicNumber).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.magicNumber).toEqual(0x5f0f3cf5);
+    }); 
+
+    it('has flags', function() {
+      expect(fontHeaderTable.flags).toEqual(jasmine.any(Array));
+      expect(fontHeaderTable.flags[0]).toEqual(true);
+      expect(fontHeaderTable.flags[1]).toEqual(false);
+      expect(fontHeaderTable.flags[2]).toEqual(false);
+      expect(fontHeaderTable.flags[3]).toEqual(true);
+      expect(fontHeaderTable.flags[4]).toEqual(false);
+      expect(fontHeaderTable.flags[5]).toEqual(false);
+      expect(fontHeaderTable.flags[6]).toEqual(false);
+      expect(fontHeaderTable.flags[7]).toEqual(false);
+      expect(fontHeaderTable.flags[8]).toEqual(false);
+      expect(fontHeaderTable.flags[9]).toEqual(false);
+      expect(fontHeaderTable.flags[10]).toEqual(false);
+      expect(fontHeaderTable.flags[11]).toEqual(false);
+      expect(fontHeaderTable.flags[12]).toEqual(false);
+      expect(fontHeaderTable.flags[13]).toEqual(false);
+      expect(fontHeaderTable.flags[14]).toEqual(false);
+      expect(fontHeaderTable.flags[15]).toEqual(false);
+    }); 
+
+    it('has unitsPerEm', function() {
+      expect(fontHeaderTable.unitsPerEm).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.unitsPerEm).toEqual(1000);
+    });
+
+    it('has created', function() {
+      expect(fontHeaderTable.created).toEqual(jasmine.any(Date));
+      expect(fontHeaderTable.created).toEqual(new Date('Fri Jan 11 10:57:01 2013 GMT+0000'));
+    });
+
+    it('has modified', function() {
+      expect(fontHeaderTable.modified).toEqual(jasmine.any(Date));
+      expect(fontHeaderTable.modified).toEqual(new Date('Fri Jan 11 18:57:02 2013 GMT+0000'));
+    });
+
+    it('has xMin', function() {
+      expect(fontHeaderTable.xMin).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.xMin).toEqual(-47);
+    });
+
+
+    it('has yMin', function() {
+      expect(fontHeaderTable.yMin).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.yMin).toEqual(-400);
+    });
+
+    it('has xMax', function() {
+      expect(fontHeaderTable.xMax).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.xMax).toEqual(706);
+    });
+
+    it('has yMax', function() {
+      expect(fontHeaderTable.yMax).toEqual(jasmine.any(Number));
+      expect(fontHeaderTable.yMax).toEqual(1000);
+    });
+
+    it('has macStyle', function() {
+      expect(fontHeaderTable.macStyle).toEqual(jasmine.any(Array));
+      expect(fontHeaderTable.macStyle[0]).toEqual(false);
+      expect(fontHeaderTable.macStyle[1]).toEqual(false);
+      expect(fontHeaderTable.macStyle[2]).toEqual(false);
+      expect(fontHeaderTable.macStyle[3]).toEqual(false);
+      expect(fontHeaderTable.macStyle[4]).toEqual(false);
+      expect(fontHeaderTable.macStyle[5]).toEqual(false);
+      expect(fontHeaderTable.macStyle[6]).toEqual(false);
+      expect(fontHeaderTable.macStyle[7]).toEqual(false);
+      expect(fontHeaderTable.macStyle[8]).toEqual(false);
+      expect(fontHeaderTable.macStyle[9]).toEqual(false);
+      expect(fontHeaderTable.macStyle[10]).toEqual(false);
+      expect(fontHeaderTable.macStyle[11]).toEqual(false);
+      expect(fontHeaderTable.macStyle[12]).toEqual(false);
+      expect(fontHeaderTable.macStyle[13]).toEqual(false);
+      expect(fontHeaderTable.macStyle[14]).toEqual(false);
+      expect(fontHeaderTable.macStyle[15]).toEqual(false);
+    });
+
+   it('has lowestRecPPEM', function() {
+     expect(fontHeaderTable.lowestRecPPEM).toEqual(jasmine.any(Number));
+     expect(fontHeaderTable.lowestRecPPEM).toEqual(9);
+   });
+
+   it('has fontDirectionHint', function() {
+     expect(fontHeaderTable.fontDirectionHint).toEqual(jasmine.any(Number));
+     expect(fontHeaderTable.fontDirectionHint).toEqual(2);
+   });
+
+   it('has indexToLocFormat', function() {
+     expect(fontHeaderTable.indexToLocFormat).toEqual(jasmine.any(Number));
+     expect(fontHeaderTable.indexToLocFormat).toEqual(0);
+   });
+
+   it('has glyphDataFormat', function() {
+     expect(fontHeaderTable.glyphDataFormat).toEqual(jasmine.any(Number));
+     expect(fontHeaderTable.glyphDataFormat).toEqual(0);
+   });
+
+  it('has isLongtLocaFormat() method', function() {
+    expect(fontHeaderTable.isLongtLocaFormat).toEqual(jasmine.any(Function));
+    expect(fontHeaderTable.isLongtLocaFormat()).toEqual(false);
+  });
+
     // TODO(ynakajima) Add detail.
+ 
 
   });
 
--- src/ttf/FontHeaderTable.js
+++ src/ttf/FontHeaderTable.js
@@ -40,12 +40,250 @@ if (!ttfjs) { var ttfjs = {}; }
   var jDataView = (typeof global.jDataView === 'undefined') ?
     require('../../vendor/jdataview') :
     global.jDataView;
+  ttfjs.Table = (typeof require !== 'undefined') ?
+    require('../Table.js').Table :
+    global.ttfjs.Table;
 
   /**
    * FontHeaderTable Class
    * @constructor
    */
   ttfjs.ttf.FontHeaderTable = function() {
+    /**
+     * Table version number.
+     * @type {number}
+     */
+    this.version = 1.0;
+
+    /**
+     * Font revision.
+     * @type {number}
+     */
+    this.fontRevision = 0;
+
+    /**
+     * CheckSum Adjustment.
+     * <pre>
+     * To compute: set it to 0, sum the entire font as ULONG,
+     * then store 0xB1B0AFBA - sum.
+     * @type {number}
+     */
+    this.checkSumAdjustment = 0;
+
+    /**
+     * MagicNumber.
+     * @type {number}
+     */
+    this.magicNumber = 0x5F0F3CF5;
+
+     /**
+     * Flags.
+     * <pre>
+     * bit 0 - y value of 0 specifies baseline
+     * bit 1 - x position of left most black bit is LSB
+     * bit 2 - scaled point size and actual point size will differ
+     *         (i.e. 24 point glyph differs from 12 point glyph scaled by factor of 2)
+     * bit 3 - use integer scaling instead of fractional
+     * bit 4 - (used by the Microsoft implementation of the TrueType scaler)
+     * bit 5 - This bit should be set in fonts that are intended to e laid out vertically,
+     *         and in which the glyphs have been drawn such that an x-coordinate of 0 corresponds to the desired vertical baseline.
+     * bit 6 - This bit must be set to zero.
+     * bit 7 - This bit should be set if the font requires layout for correct linguistic rendering (e.g. Arabic fonts).
+     * bit 8 - This bit should be set for a GX font which has one or more metamorphosis effects designated as happening by default.
+     * bit 9 - This bit should be set if the font contains any strong right-to-left glyphs.
+     * bit 10 - This bit should be set if the font contains Indic-style rearrangement effects.
+     * Bit 11: Font data is 'lossless,' as a result of having been compressed and decompressed with the Agfa MicroType Express engine.
+     * Bit 12: Font converted (produce compatible metrics)
+     * Bit 13: Font optimized for ClearType™. Note,
+     *         fonts that rely on embedded bitmaps (EBDT) for rendering should not be
+     *         considered optimized for ClearType, and therefore should keep this bit cleared.
+     * Bit 14: Last Resort font. If set, indicates that the glyphs encoded in the cmap subtables
+     *         are simply generic symbolic representations of code point ranges and don’t truly
+     *         represent support for those code points. If unset, indicates that the glyphs encoded
+     *         in the cmap subtables represent proper support for those code points.
+     * Bit 15: Reserved, set to 0 
+     * </pre>
+     * @type {number}
+     */
+    this.flags = [
+      false, false, false, false,
+      false, false, false, false,
+      false, false, false, false,
+      false, false, false, false];
+
+    /**
+     * Units per EM.
+     * <pre>Valid range is from 16 to 16384</pre>
+     * @type {number}
+     */
+    this.unitsPerEm = 0x5F0F3CF5;
+
+    /**
+     * Created DateTime.
+     * @type {Date}
+     */
+    this.created = new Date('January 1, 1904 00:00:00');
+
+    /**
+     * Modified DateTime.
+     * @type {Date}
+     */
+    this.modified = new Date('January 1, 1904 00:00:00');
+
+    /**
+     * Minimum value of X for all glyph bounding boxes.
+     * @type {number}
+     */
+    this.xMin = 0;
+
+    /**
+     * Minimum value of Y for all glyph bounding boxes.
+     * @type {number}
+     */
+    this.yMin = 0;
+
+    /**
+     * Maximum value of X for all glyph bounding boxes.
+     * @type {number}
+     */
+    this.xMax = 0;
+
+    /**
+     * Maximum value of Y for all glyph bounding boxes.
+     * @type {number}
+     */
+    this.yMax = 0;
+
+    /**
+     * Mac Style
+     * <pre>
+     * bit 0 bold
+     * bit 1 italic
+     * bit 2 underline
+     * bit 3 outline
+     * bit 4 shadow
+     * bit 5 condensed (narrow)
+     * bit 6 extended
+     * </pre>
+     * @type {Array.<Boolean>}
+     */
+    this.macStyle = [
+      false, false, false, false,
+      false, false, false, false,
+      false, false, false, false,
+      false, false, false, false];
+
+    /**
+     * Lowest RecPPEM.
+     * Smallest readable size in pixels.
+     * @type {number}
+     */
+    this.lowestRecPPEM = 0;
+
+    /**
+     * fontDirectionHint.
+     * <pre>
+     * 0 Fully mixed directional glyphs;
+     * 1 Only strongly left to right;
+     * 2 Like 1 but also contains neutrals1;
+     * -1 Only strongly right to left;
+     * -2 Like -1 but also contains neutrals.
+     * </pre>
+     * @type {number}
+     */
+    this.fontDirectionHint = 0;
+
+    /**
+     * Index to Location format.
+     * <pre>0 for short offsets, 1 for long.</pre>
+     * @type {number}
+     */
+    this.indexToLocFormat = 0;
+
+    /**
+     * Glyphi Data Format.
+     * <pre>0 for current format.</pre>
+     * @type {number}
+     */
+    this.glyphDataFormat = 0;
+
+  };
+
+  ttfjs.ttf.FontHeaderTable.prototype = new ttfjs.Table();
+
+  /**
+   * Return Loca Format.
+   * @return {Boolean} if Loca Format is Long: true.
+   */
+  ttfjs.ttf.FontHeaderTable.prototype.isLongtLocaFormat = function() {
+    return (this.indexToLocFormat === 1);
+  };
+
+  /**
+   * Create Table insance from TTFDataView object.
+   * @param {TTFDataView} view TTFDataView object.
+   * @param {number} offset offset.
+   */
+  ttfjs.ttf.FontHeaderTable.createFromDataView = function(view, offset) {
+    view.seek(offset);
+    
+    // init fontHeaderTable
+    var fontHeaderTable = new ttfjs.ttf.FontHeaderTable();
+
+    // version
+    fontHeaderTable.version = view.getFixed();
+
+    // fontRevision
+    fontHeaderTable.fontRevision = view.getFixed();
+
+    // checkSumAdjustment
+    fontHeaderTable.checkSumAdjustment = view.getUlong();
+
+    // magickNumber
+    fontHeaderTable.magickNumber = view.getUlong();
+
+    // flags
+    var flags = view.getUshort();
+    for (var i = 0; i < 16; i++) {
+      var bit = Math.pow(2, i);
+      fontHeaderTable.flags[i] = ((bit & flags) === bit);
+    }
+
+    // unitsPerEm
+    fontHeaderTable.unitsPerEm = view.getUshort();
+
+    // created
+    fontHeaderTable.created = view.getLongDateTime();
+
+    // modified
+    fontHeaderTable.modified = view.getLongDateTime();
+
+    // xMin, yMin, xMax, yMax
+    fontHeaderTable.xMin = view.getFWord();
+    fontHeaderTable.yMin = view.getFWord();
+    fontHeaderTable.xMax = view.getFWord();
+    fontHeaderTable.yMax = view.getFWord();
+ 
+    // macStyle
+    var macStyle = view.getUshort();
+    for (var i = 0; i < 16; i++) {
+      var bit = Math.pow(2, i);
+      fontHeaderTable.macStyle[i] = ((bit & macStyle) === bit);
+    }
+
+    // lowestRecPPEM
+    fontHeaderTable.lowestRecPPEM = view.getUshort();
+ 
+    // fontDirectionHint
+    fontHeaderTable.fontDirectionHint = view.getShort();
+ 
+    // indexToLocFormat 
+    fontHeaderTable.indexToLocFormat = view.getShort();
+ 
+    // glyphDataFormat 
+    fontHeaderTable.glyphDataFormat = view.getShort();
+   
+    return fontHeaderTable;
   };
 
   // TODO(ynakajima): Implement this.
