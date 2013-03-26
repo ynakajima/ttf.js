c378890d1b9247b0b2ad90ce92d2f92a37fc4112
ynakajima <yuhta.nakajima@gmail.com>
2013-03-27 02:34:49 +0900

    Implement maxp - MaximumProfileTable Class

--- src/ttf/MaximumProfileTable.js
+++ src/ttf/MaximumProfileTable.js
@@ -24,7 +24,7 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * </pre>
  *
- * @fileoverview MaximumProfileTable Class file.
+ * @fileoverview maxp - MaximumProfileTable Class file.
  * @author yuhta.nakajima@gmail.com (ynakajima)
  */
 
@@ -41,14 +41,178 @@ if (!ttfjs) { var ttfjs = {}; }
     require('../../vendor/jdataview') :
     global.jDataView;
 
+  ttfjs.Table = (typeof require !== 'undefined') ?
+    require('../Table.js').Table :
+    global.ttfjs.Table;
+
   /**
-   * MaximumProfileTable Class
+   * maxp - MaximumProfileTable Class
    * @constructor
+   * @param {String} specName name of table spec.
    */
-  ttfjs.ttf.MaximumProfileTable = function() {
+  ttfjs.ttf.MaximumProfileTable = function(specName) {
+
+    /**
+     * Table Spec.
+     * @type {Array}
+     */
+    this.specs = [
+      {
+        name: 'v0_5',
+        dataSpec: [
+          {name: 'tableVersion', type: 'Fixed'},
+          {name: 'numGlyphs', type: 'USHORT'}
+        ]
+      },
+      {
+        name: 'v1_0',
+        dataSpec: [
+          {name: 'tableVersion', type: 'Fixed'},
+          {name: 'numGlyphs', type: 'USHORT'},
+          {name: 'maxPoints', type: 'USHORT'},
+          {name: 'maxContours', type: 'USHORT'},
+          {name: 'maxCompositePoints', type: 'USHORT'},
+          {name: 'maxCompositeContours', type: 'USHORT'},
+          {name: 'maxZones', type: 'USHORT'},
+          {name: 'maxTwilightPoints', type: 'USHORT'},
+          {name: 'maxStorage', type: 'USHORT'},
+          {name: 'maxFunctionDefs', type: 'USHORT'},
+          {name: 'maxInstructionDefs', type: 'USHORT'},
+          {name: 'maxStackElements', type: 'USHORT'},
+          {name: 'maxSizeOfInstructions', type: 'USHORT'},
+          {name: 'maxComponentElements', type: 'USHORT'},
+          {name: 'maxComponentDepth', type: 'USHORT'}
+        ]
+      }
+    ];
+
+    ttfjs.Table.call(this, specName);
+
+    /**
+     * Table table version number.
+     * @type {number}
+     */
+    this.tableVersion = 0;
+
+    /**
+     * The number of glyphs in the font.
+     * @type {number}
+     */
+    this.numGlyphs = 0;
+
+    /**
+     * Maximum points in a non-composite glyph.
+     * @type {number}
+     */
+    this.maxPoints = 0;
+
+    /**
+     * Maximum contours in a non-composite glyph.
+     * @type {number}
+     */
+    this.maxContours = 0;
+
+    /**
+     * Maximum points in a composite glyph.
+     * @type {number}
+     */
+    this.maxCompositePoints = 0;
+
+    /**
+     * Maximum contours in a composite glyph.
+     * @type {number}
+     */
+    this.maxCompositeContours = 0;
+
+    /**
+     * 1 if instructions do not use the twilight zone (Z0),
+     * or 2 if instructions do use Z0; should be set to 2 in most cases.
+     * @type {number}
+     */
+    this.maxZones = 0;
+
+    /**
+     * Maximum points used in Z0.
+     * @type {number}
+     */
+    this.maxTwilightPoints = 0;
+
+    /**
+     * Number of Storage Area locations.
+     * @type {number}
+     */
+    this.maxStorage = 0;
+
+    /**
+     * Number of FDEFs.
+     * @type {number}
+     */
+    this.maxFunctionDefs = 0;
+
+    /**
+     * Number of IDEFs.
+     * @type {number}
+     */
+    this.maxInstructionDefs = 0;
+
+    /**
+     * Maximum stack depth2.
+     * @type {number}
+     */
+    this.maxStackElements = 0;
+
+    /**
+     * Maximum byte count for glyph instructions.
+     * @type {number}
+     */
+    this.maxSizeOfInstructions = 0;
+
+    /**
+     * Maximum number of components referenced at "top level"
+     * for any composite glyph.
+     * @type {number}
+     */
+    this.maxComponentElements = 0;
+
+    /**
+     * Maximum levels of recursion; 1 for simple components.
+     * @type {number}
+     */
+    this.maxComponentDepth = 0;
+
   };
 
-  // TODO(ynakajima): Implement this.
+  // extends Table
+  ttfjs.ttf.MaximumProfileTable.prototype = new ttfjs.Table();
+
+  /**
+   * Create MaximumProfileTable insance from TTFDataView object.
+   * @param {TTFDataView} view TTFDataView object.
+   * @param {number} offset offset.
+   * @return {ttfjs.ttf.MaximumProfileTable} incetance of MaximumProfileTable.
+   */
+  ttfjs.ttf.MaximumProfileTable.createFromDataView = function(view, offset) {
+
+    var tableVersion = view.getFixed(offset);
+    var specName = '';
+
+    switch (tableVersion) {
+      case 0.5:
+        specName = 'v0_5';
+        break;
+
+      case 1.0:
+        specName = 'v1_0';
+        break;
+
+      default:
+        throw 'err: invalid table version.';
+    }
+
+    return ttfjs.Table.createFromDataView_(
+           ttfjs.ttf.MaximumProfileTable, view, offset, specName);
+  };
+
 
   // exports
   if (typeof module !== 'undefined') {
